46855ae6ab9c7b195bb3715869beff2d58af5643
muhdismail3271 <muhammedismail73271@gmail.com>
2026-02-18 14:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (46.76%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let arr = s.split(` `);
+    while(arr[0]===``){
+        arr.shift()
+    }
+    while(arr[arr.length-1]===``){
+        arr.pop()
+    }
+    return arr[arr.length-1].length;
+};
