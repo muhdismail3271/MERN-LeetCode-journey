df2874920be3a3169ad767d3b593b1ec9f0f8d69
muhdismail3271 <muhammedismail73271@gmail.com>
2026-02-19 09:54:29 +0530

    Time: 2209 ms (5.24%) | Memory: 63.6 MB (5.35%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for(let i=0; i<s.length; i++){
+        s.unshift(s[i]);
+        s.splice(i+1,1);
+    }
+    return s;
+};
