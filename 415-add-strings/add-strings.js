dd02b4dcf02538f149b10c00a9ce4c99ae9bb4cf
muhdismail3271 <muhammedismail73271@gmail.com>
2026-03-03 10:26:27 +0530

    Time: 1 ms (85.26%) | Memory: 54.4 MB (82.59%) - LeetSync

--- 415-add-strings/add-strings.js
+++ 415-add-strings/add-strings.js
@@ -4,6 +4,8 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-    let result = BigInt(num1) + BigInt(num2)
+    let n1 = BigInt(num1)
+    let n2 = BigInt(num2)
+    let result = BigInt(n1+n2);
     return result.toString();
 };
